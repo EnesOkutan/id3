254140e82aee921723328bab1ffe45d318b6cb26
Enes Okutan <enes.okutan@hotmail.com>
2019-06-13 02:43:53 +0000

    Added entropy function

--- id3.py
+++ id3.py
@@ -1,14 +1,31 @@
 import sys
 import pandas as pd
+import math
+import os
 
 def load_file(file):
-    data = pd.read_csv(file)
-    return data
+    dataset = pd.read_csv(file)
+    return dataset
+
+def entropy(target):
+    ent = 0
+    result = {}
+    target_n = len(target)
+    for target_data in target:
+        if target_data in result:
+            result[target_data] += 1
+        else:
+            result[target_data] = 1
+    for label in result.keys():
+        p_x = result[label] / target_n
+        ent += - p_x * math.log(p_x,2)
+    return ent
 
 def main():
     argv = sys.argv
-    data = load_file(argv[1])
-    print(data)
+    dataset = load_file(argv[1])
+    target = dataset[dataset.keys()[-1]]
+    del dataset[dataset.keys()[-1]]
 
 if __name__ == "__main__":
     main()
