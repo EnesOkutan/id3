f0f207638b04001512c01b498fcd9a094046a029
Enes Okutan <enes.okutan@hotmail.com>
2019-06-13 02:48:16 +0000

    Added id3 function to implement the id3 algorithm

--- id3.py
+++ id3.py
@@ -58,11 +58,38 @@ def common_target(target_data):
             target = data
     return target
 
+def id3(data, target):
+    node = {}
+    if len(data) == 0:
+        return {'result': common_target(target)}
+    max_winnings = None
+    max_attribute = None
+    max_attribute_data = None
+    max_target_data = None
+    general_entropy = entropy(target)
+    if general_entropy == 0:
+        return {'result': common_target(target)}
+    for attribute in data:
+        attribute_data, target_data = partition(data, attribute, target)
+        avg_entropy = avg_entropy_partitions(target_data)
+        winning = general_entropy - avg_entropy
+        if max_winnings is None or winning > max_winnings:
+            max_winnings = winning
+            max_attribute = attribute
+            max_attribute_data = attribute_data
+            max_target_data = target_data
+    node[max_attribute] = {}
+    for attr_data in max_attribute_data:
+        del max_attribute_data[attr_data][max_attribute]
+        node[max_attribute][attr_data] = id3(max_attribute_data[attr_data], max_target_data[attr_data])
+    return node
+
 def main():
     argv = sys.argv
     dataset = load_file(argv[1])
     target = dataset[dataset.keys()[-1]]
     del dataset[dataset.keys()[-1]]
+    node = id3(dataset,target)
 
 if __name__ == "__main__":
     main()
